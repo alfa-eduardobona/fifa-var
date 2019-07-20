5364bbdf501659d4843ca507bc7906bf8df2ca0b
Eduardo Bona <eduardobona@vivaweb.net>
2019-07-20 14:51:33 -0300

    primeiro commit para aula de sabado 20-07-19

---
+++ module/Application/src/Entity/Arbitro.php
@@ -0,0 +1,56 @@
+<?php
+namespace Application\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="\Application\Repository\PostRepository")
+ * @ORM\Table(name="arbitro")
+ */
+class Arbitro
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /** 
+     * @ORM\Column(name="nome", type="string", length=255)
+     */
+    protected $nome;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+}
+
